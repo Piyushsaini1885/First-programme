76ab8e81af35f8585562edf59f1bfa120e16b994
Piyushsaini1885 <2003piyushsaini@gmail.com>
2026-01-03 22:24:20 +0530

    Add two numbers
    this is my basic programme to add two numbers. Its my practice on the bases

---
+++ Add two numbers.py
@@ -0,0 +1,6 @@
+# Program to add two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+print("Sum of", num1, "and", num2, "is:", addition)
